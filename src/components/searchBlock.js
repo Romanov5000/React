cf2cf79fe6177f459aa206c5a4d475492799ab23
Romanov5000 <romanovvladislav97@gmail.com>
2020-06-13 14:57:15 +0300

    add todo_list in project

---
+++ src/components/searchBlock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBlock = () => {
+    const searchText = 'event lox';
+    return (
+        <input placeholder={searchText} />
+    )
+}
+export default SearchBlock;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1> react element</h1>
+import SearchBlock from './components/searchBlock';
 
-ReactDOM.render(el, document.getElementById('root'));
+const TodoList = () => {
+  const items = ['learn html', 'learn css', 'learn js', 'learn react']
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+      <li>{items[2]}</li>
+      <li>{items[3]}</li>
+
+    </ul>
+  )
+}
+
+const Header = () => {
+  let st = {
+    color : 'red',
+    fontSize : ' 40px',
+  }
+  return (
+    <h1 style={st} className = 'header'>todo list</h1>
+  )
+}
+
+const App = () => {
+  const isLoggen = true;
+  const loginBlock = <span>sign in</span>
+  const welcome = <span>Hello</span>
+  return (
+    <div>
+      {isLoggen ? welcome : loginBlock}
+      <Header />
+      <SearchBlock />
+      <TodoList />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
