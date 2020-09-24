52d674975829816766bf421887454d329d0ced50
Romanov5000 <romanovvladislav97@gmail.com>
2020-09-24 21:19:58 +0300

    Updates

---
+++ precache-manifest.e5dac8c2dd7eed36a9f92ecfd1ff1d90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa5f53cdb7bb326b9bb71e7f4f647d3",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "a4cadaa38a16b3ad4651",
+    "url": "/ToDoList/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bc3c431a587c28b9dbf9",
+    "url": "/ToDoList/static/css/main.06490f6e.chunk.css"
+  },
+  {
+    "revision": "a4cadaa38a16b3ad4651",
+    "url": "/ToDoList/static/js/2.6fd3e516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.6fd3e516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc3c431a587c28b9dbf9",
+    "url": "/ToDoList/static/js/main.87b9f788.chunk.js"
+  },
+  {
+    "revision": "1a7f432604eba6b25960",
+    "url": "/ToDoList/static/js/runtime-main.ea480887.js"
+  }
+]);
